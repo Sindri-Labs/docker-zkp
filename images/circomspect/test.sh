fa9bacae6f9dd047333a9c1b342acdde0292a24c
Evan Sangaline <esangaline@gmail.com>
2024-02-24 16:28:40 -0700

    Further simplify the circomspect test script.

--- images/circomspect/test.sh
+++ images/circomspect/test.sh
@@ -1,10 +1,7 @@
 #! /bin/sh -e
 
-# Analyze circuit.
-circomspect -l INFO -v --allow CS0003 --allow CS0004 --allow CS0005 --allow CS0010 --allow P1004 circuit.circom
-
 # Show help information.
 circomspect --help
 
-# Output analysis results to a Sarif file.
-circomspect -s analysis.sarif circuit.circom
+# Analyze circuit and write results to a Sarif file.
+circomspect -l INFO -v --allow CS0003 --allow CS0004 --allow CS0005 --allow CS0010 --allow P1004 -s analysis.sarif circuit.circom
