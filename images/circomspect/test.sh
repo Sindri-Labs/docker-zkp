df05da29f6e596959e06993efdc5799a0f03db0f
Evan Sangaline <esangaline@gmail.com>
2024-02-24 16:22:02 -0700

    Stop checking each curve, it's not necessary.

--- images/circomspect/test.sh
+++ images/circomspect/test.sh
@@ -6,10 +6,5 @@ circomspect -l INFO -v --allow CS0003 --allow CS0004 --allow CS0005 --allow CS00
 # Show help information.
 circomspect --help
 
-# Test analysis with each supported curve.
-for curve in BN254 BLS12_381 GOLDILOCKS; do
-    circomspect -c $curve --allow CS0003 --allow CS0004 --allow CS0005 --allow CS0010 --allow P1004 circuit.circom
-done
-
 # Output analysis results to a Sarif file.
 circomspect -s analysis.sarif circuit.circom
