7ab6d790673883d84d0c5cd464a51050ea330649
Evan Sangaline <esangaline@gmail.com>
2024-02-24 16:01:07 -0700

    Allow CS0004 with circomspect.

--- images/circomspect/test.sh
+++ images/circomspect/test.sh
@@ -1,14 +1,14 @@
 #! /bin/sh -e
 
 # Analyze circuit.
-circomspect -l INFO -v circuit.circom
+circomspect -l INFO -v --allow CS0004 circuit.circom
 
 # Show help information.
 circomspect --help
 
 # Test analysis with each supported curve.
 for curve in BN254 BLS12_381 GOLDILOCKS; do
-    circomspect -c $curve circuit.circom
+    circomspect -c $curve --allow CS0004 circuit.circom
 done
 
 # Output analysis results to a Sarif file.
