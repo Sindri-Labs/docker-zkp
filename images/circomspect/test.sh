d7b68f67a8bdadf7f9ee14c4942ce2221494a191
Evan Sangaline <esangaline@gmail.com>
2024-02-24 16:48:03 -0700

    Allow another violation.

--- images/circomspect/test.sh
+++ images/circomspect/test.sh
@@ -4,4 +4,4 @@
 circomspect --help
 
 # Analyze circuit and write results to a Sarif file.
-circomspect -l INFO -v --allow CS0003 --allow CS0004 --allow CS0005 --allow CS0010 --allow P1004 -s analysis.sarif circuit.circom
+circomspect -l INFO -v --allow CS0003 --allow CS0004 --allow CS0005 --allow CS0010 --allow CS0014 --allow P1004 -s analysis.sarif circuit.circom
