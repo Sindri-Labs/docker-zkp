c7970595974b8c8f6eb06a446c62620bcf2b821f
Evan Sangaline <esangaline@gmail.com>
2024-02-24 18:20:22 -0700

    Add production deployment for circomspect
    This adds a new github actions workflow which builds and deploys optimized
    images for every circomspect tag. This runs whenever a PR is delivered, and
    also runs nightly at 3 or 4am Eastern. The workflow can also be run manually.
    The PR job now deploys all of the images with a `dev` tag instead of `latest`.
    
    Merges #8

--- images/circomspect/test.sh
+++ images/circomspect/test.sh
@@ -1,15 +1,7 @@
 #! /bin/sh -e
 
-# Analyze circuit.
-circomspect -l INFO -v circuit.circom
-
 # Show help information.
 circomspect --help
 
-# Test analysis with each supported curve.
-for curve in BN254 BLS12_381 GOLDILOCKS; do
-    circomspect -c $curve circuit.circom
-done
-
-# Output analysis results to a Sarif file.
-circomspect -s analysis.sarif circuit.circom
+# Analyze circuit and write results to a Sarif file.
+circomspect -l INFO -v --allow CS0003 --allow CS0004 --allow CS0005 --allow CS0010 --allow CS0014 --allow P1004 -s analysis.sarif circuit.circom
