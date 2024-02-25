32d77ec2d9837f5289011e81020c12845ad85a71
Evan Sangaline <esangaline@gmail.com>
2024-02-25 11:17:44 -0700

    Minor naming cleanup.

--- images/snarkjs/test.sh
+++ images/snarkjs/test.sh
@@ -14,31 +14,31 @@ mkdir /tmp/snarkjs/
 mkdir /tmp/snarkjs/r1cs
 
 # Print R1CS file metadata (number of constraints, inputs, etc.)
-snarkjs r1cs info circom_files/circuit.r1cs
+snarkjs r1cs info circom-files/circuit.r1cs
 
 # Print binary file metadata
-snarkjs file info circom_files/circuit.r1cs 
+snarkjs file info circom-files/circuit.r1cs 
 
 # List all of the constraints in the circuit
-snarkjs r1cs print circom_files/circuit.r1cs circom_files/circuit.sym
+snarkjs r1cs print circom-files/circuit.r1cs circom-files/circuit.sym
 
 # Transpile R1CS format into a human-readable JSON file
-snarkjs r1cs export json circom_files/circuit.r1cs /tmp/snarkjs/r1cs/output.json
+snarkjs r1cs export json circom-files/circuit.r1cs /tmp/snarkjs/r1cs/output.json
 
 # Witness Utilities
 mkdir /tmp/snarkjs/wtns
 
 # Use Snarkjs CLI witness generator
-snarkjs wtns calculate circom_files/circuit.wasm  circom_files/input.json /tmp/snarkjs/wtns/witness.wtns
+snarkjs wtns calculate circom-files/circuit.wasm  circom-files/input.json /tmp/snarkjs/wtns/witness.wtns
 
 # Use Snarkjs CLI witness generator in debug mode
-snarkjs wtns debug circom_files/circuit.wasm  circom_files/input.json /tmp/snarkjs/wtns/witness.wtns circom_files/circuit.sym
+snarkjs wtns debug circom-files/circuit.wasm  circom-files/input.json /tmp/snarkjs/wtns/witness.wtns circom-files/circuit.sym
 
 # Turn witness file into human-readable JSON
 snarkjs wtns export json /tmp/snarkjs/wtns/witness.wtns /tmp/snarkjs/wtns/witness.json
 
 # Validate a witness file against a circuit's R1CS file
-snarkjs wtns check circom_files/circuit.r1cs circom_files/witness.wtns
+snarkjs wtns check circom-files/circuit.r1cs circom-files/witness.wtns
 
 
 # Run a small mock trusted setup for bn128 and bls12-381
@@ -78,7 +78,7 @@ for curve in BN254 BLS12-381; do
         mkdir /tmp/snarkjs/BN254/groth16/
 
         # Initialize circuit specific key
-        snarkjs groth16 setup circom_files/circuit.r1cs /tmp/snarkjs/BN254/final.ptau /tmp/snarkjs/BN254/groth16/circuit_1.zkey
+        snarkjs groth16 setup circom-files/circuit.r1cs /tmp/snarkjs/BN254/final.ptau /tmp/snarkjs/BN254/groth16/circuit_1.zkey
 
         # TESTING ZKEY UTILITIES
         snarkjs zkey contribute /tmp/snarkjs/BN254/groth16/circuit_1.zkey /tmp/snarkjs/BN254/groth16/circuit_2.zkey -e="input"
@@ -89,7 +89,7 @@ for curve in BN254 BLS12-381; do
         snarkjs zkey import bellman /tmp/snarkjs/BN254/groth16/circuit_2.zkey /tmp/snarkjs/BN254/groth16/response_phase2_3 /tmp/snarkjs/BN254/groth16/circuit_3.zkey
 
         # Verify the zkey matches the initial trusted setup and circuit
-        snarkjs zkey verify circom_files/circuit.r1cs /tmp/snarkjs/BN254/final.ptau /tmp/snarkjs/BN254/groth16/circuit_3.zkey
+        snarkjs zkey verify circom-files/circuit.r1cs /tmp/snarkjs/BN254/final.ptau /tmp/snarkjs/BN254/groth16/circuit_3.zkey
 
         # Apply public source of randomness
         snarkjs zkey beacon /tmp/snarkjs/BN254/groth16/circuit_3.zkey /tmp/snarkjs/BN254/groth16/final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10
@@ -101,13 +101,13 @@ for curve in BN254 BLS12-381; do
         snarkjs zkey export verificationkey /tmp/snarkjs/BN254/groth16/final.zkey /tmp/snarkjs/BN254/groth16/verification_key.json
 
         # End-to-end proof (no preconstructed witness)
-        snarkjs groth16 fullprove circom_files/input.json circom_files/circuit.wasm /tmp/snarkjs/BN254/groth16/final.zkey /tmp/snarkjs/BN254/groth16/proofA.json /tmp/snarkjs/BN254/groth16/publicA.json
+        snarkjs groth16 fullprove circom-files/input.json circom-files/circuit.wasm /tmp/snarkjs/BN254/groth16/final.zkey /tmp/snarkjs/BN254/groth16/proofA.json /tmp/snarkjs/BN254/groth16/publicA.json
 
         # Verify the proof
         snarkjs groth16 verify /tmp/snarkjs/BN254/groth16/verification_key.json /tmp/snarkjs/BN254/groth16/publicA.json /tmp/snarkjs/BN254/groth16/proofA.json
 
         # Create a proof from a serialized witness
-        snarkjs groth16 prove /tmp/snarkjs/BN254/groth16/final.zkey circom_files/witness.wtns /tmp/snarkjs/BN254/groth16/proofB.json /tmp/snarkjs/BN254/groth16/publicB.json
+        snarkjs groth16 prove /tmp/snarkjs/BN254/groth16/final.zkey circom-files/witness.wtns /tmp/snarkjs/BN254/groth16/proofB.json /tmp/snarkjs/BN254/groth16/publicB.json
 
         # Verify that proof
         snarkjs groth16 verify /tmp/snarkjs/BN254/groth16/verification_key.json /tmp/snarkjs/BN254/groth16/publicB.json /tmp/snarkjs/BN254/groth16/proofB.json
@@ -124,19 +124,19 @@ for curve in BN254 BLS12-381; do
                 mkdir /tmp/snarkjs/$curve/$backend/
 
                 # Generate the proving key from powers of tau file
-                snarkjs $backend setup circom_files/circuit.r1cs /tmp/snarkjs/$curve/final.ptau /tmp/snarkjs/$curve/$backend/circuit.zkey
+                snarkjs $backend setup circom-files/circuit.r1cs /tmp/snarkjs/$curve/final.ptau /tmp/snarkjs/$curve/$backend/circuit.zkey
 
                 # Save a copy of the verification key
                 snarkjs zkey export verificationkey /tmp/snarkjs/$curve/$backend/circuit.zkey /tmp/snarkjs/$curve/$backend/verification_key.json
 
                 # End-to-end proof (no preconstructed witness)
-                snarkjs $backend fullprove circom_files/input.json circom_files/circuit.wasm /tmp/snarkjs/$curve/$backend/circuit.zkey /tmp/snarkjs/$curve/$backend/proofA.json /tmp/snarkjs/$curve/$backend/publicA.json
+                snarkjs $backend fullprove circom-files/input.json circom-files/circuit.wasm /tmp/snarkjs/$curve/$backend/circuit.zkey /tmp/snarkjs/$curve/$backend/proofA.json /tmp/snarkjs/$curve/$backend/publicA.json
 
                 # Verify the proof
                 snarkjs $backend verify /tmp/snarkjs/$curve/$backend/verification_key.json /tmp/snarkjs/$curve/$backend/publicA.json /tmp/snarkjs/$curve/$backend/proofA.json
 
                 # Create a proof from a serialized witness
-                snarkjs $backend prove /tmp/snarkjs/$curve/$backend/circuit.zkey circom_files/witness.wtns /tmp/snarkjs/$curve/$backend/proofB.json /tmp/snarkjs/$curve/$backend/publicB.json
+                snarkjs $backend prove /tmp/snarkjs/$curve/$backend/circuit.zkey circom-files/witness.wtns /tmp/snarkjs/$curve/$backend/proofB.json /tmp/snarkjs/$curve/$backend/publicB.json
 
                 # Verify that proof
                 snarkjs $backend verify /tmp/snarkjs/$curve/$backend/verification_key.json /tmp/snarkjs/$curve/$backend/publicB.json /tmp/snarkjs/$curve/$backend/proofB.json
